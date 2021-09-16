4bb411a56a7ee886d4b5144191cdb7c3e201940b
kennie-larkson <materialkenny@gmail.com>
2021-09-16 15:31:11 +0100

    refined the note component

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-const Note = ({note}) => {
+const Note = ({ note }) => {
   return (
-    <div className="note">
-    {note.title}
+    <div className="note white">
+      <div className="right-align">
+        <i className="material-icons red-text" style={{ cursor: "pointer" }}>
+          favorite
+        </i>
+        <i className="material-icons" style={{ cursor: "pointer" }}>
+          delete
+        </i>
+      </div>
+      <h5 className="black-text">{note.title}</h5>
+      <p className="truncate">{note.content}</p>
+      <p className="grey-text">2 days ago</p>
+      <div className="right-align">
+        <i className="material-icons black-text" style={{ cursor: "pointer" }}>edit</i>
+      </div>
     </div>
   );
 };
